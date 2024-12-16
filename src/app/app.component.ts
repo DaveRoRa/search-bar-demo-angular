57bad49d685932f167648843547c3902dee9a8a3
DaveRoRa <ftdrodriguezr@gmail.com>
2024-12-16 15:18:54 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { ClickOutsideDirective } from './click-outside.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ClickOutsideDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  isMenuOpened = signal<boolean>(false);
+  toggleMenu() {
+    this.isMenuOpened.update((prevVal) => !prevVal);
+  }
+  clickOutside(): void {
+    this.isMenuOpened.set(false);
+  }
 }
